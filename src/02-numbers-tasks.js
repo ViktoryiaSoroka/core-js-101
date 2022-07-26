e9d8d6672c38e7993c773a952fea2e40e6c6de18
Viktoryia Soroka <v.soroka@live.com>
2022-07-27 00:42:14 +0200

    fix: change function 02

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -201,8 +201,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n / 2 + 1; i + 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n !== 1;
 }
 
 /**
@@ -220,11 +225,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  if (Number(value) === Number) {
-    return value;
-  }
-  return def;
+function toNumber(/* value, def */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
